6cbcba821b87befe596ecf5892c881ca5ce56cca
Derick Hooley <dghooley@gmail.com>
2022-09-19 18:40:04 -0500

    Installed Lodash and dummyData

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,21 @@
 const graphql = require('graphql');
+var _ = require('lodash')
+
+//Dummy Data
+var usersData = [
+    { id: '1', name: 'Thompson', age: 42 },
+    { id: '13', name: 'Alvarez', age: 38 },
+    { id: '311', name: 'Errup', age: 27 },
+    { id: '19', name: 'Runner', age: 25 },
+    { id: '150', name: 'Williams', age: 58 },
+];
+
+
+
+
+
+
+
 const {
     GraphQLObjectType,
     GraphQLID,
@@ -7,6 +24,8 @@ const {
     GraphQLSchema
 
 } = graphql
+
+
 
 //Create types
 const UserType = new GraphQLObjectType({
@@ -28,14 +47,21 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
 
-
             resolve(parent, args) {
+                return _.find(usersData, { id: args.id })
+
                 //we resolve with data
-                //get and return data from data source
+                //get and return data from a data source
             }
         }
     }
 });
+/*
+{
+    user(id: "1") {
+        name
+
+*/
 
 module.exports = new GraphQLSchema({
     query: RootQuery
